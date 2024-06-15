6b3cbc6fa4ae17890df2b33abc4f39c5c61ae5de
seoyeom542 <60180664+seoyeom542@users.noreply.github.com>
2024-06-15 18:10:46 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,12 @@
+function solution(m) {
+    let al = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let a = [];
+    a.length = 52;
+    a.fill(0);
+
+    m.split("").map((n) => {
+        a[al.indexOf(n)] += 1
+    })
+
+    return a;
+}
