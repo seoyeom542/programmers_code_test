936abbe9754e6497d8f7bc027e8f65b1971cbea4
seoyeom542 <60180664+seoyeom542@users.noreply.github.com>
2024-06-17 09:40:05 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 17.68 ms, Memory: 36 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,13 @@
+function solution(arr, queries) {
+    queries.forEach((query, idx) => {
+        const [s, e, k] = query;
+        for(let i = s; i <= e; i++) {
+            if(i % k === 0) {
+                arr[i] += 1;
+            }
+        }
+    })
+    
+
+    return arr;
+}
