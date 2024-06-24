8d1a10a13f79d1f3010fb16ed4db254d41514bba
seoyeom542 <60180664+seoyeom542@users.noreply.github.com>
2024-06-24 20:44:01 +0900

    [level 0] Title: 2의 영역, Time: 2.71 ms, Memory: 40.4 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -6,11 +6,7 @@ function solution(arr) {
         if(a == 2) answer.push(i)
     })
     
-    console.log('answer: ', answer)
-    console.log(answer[0], answer[answer.length - 1])
-    if(answer.length == 1){
-        return arr.splice(answer[0], 1)
-    }
+    if(answer.length == 1) return arr.splice(answer[0], 1)
     else{
         let end = answer[answer.length - 1] - answer[0] + 1;
         return arr.splice(answer[0], end)
